eb34d78264664f1cdda56e5f0cb34c0aa8318250
S-Masakatsu <s.masakatsu197@gmail.com>
2019-12-04 16:43:53 +0900

    Add Simple signaling server

--- src/signaling.js
+++ src/signaling.js
@@ -1,1 +1,25 @@
 'use strict'
+
+import { Server as WebSocketServer } from 'ws'
+const port = 8080;
+const wsServer = new WebSocketServer({port: port});
+
+wsServer.on('connection', sock => {
+    console.log('-- websocket connected --');
+    sock.on('message', message => {
+        wsServer.clients.forEach( client => {
+            if(isSame(sock, client)) {
+                console.log('- skip sender -');
+            } else {
+                client.send(message);
+            }
+        });
+    });
+});
+
+const isSame = (ws1, ws2) => {
+    // -- compare object --
+    return (ws1 === ws2);
+}
+
+console.log('websocket server start. port=' + port);
