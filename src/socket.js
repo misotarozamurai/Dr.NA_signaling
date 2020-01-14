0f815032a9ea07c1f85bcf5028e0d6cab39f1718
Aoi Satonaka <fireball0821@icloud.com>
2020-01-15 00:54:19 +0900

    [fix] typo method name

--- src/socket.js
+++ src/socket.js
@@ -6,7 +6,7 @@ import config from 'config';
 
 import * as srv from './server'
 
-const httpsServer = srv.httpServer();
+const httpsServer = srv.httpsServer();
 
 const wsServer = new WebSocketServer({
     server : httpsServer,
