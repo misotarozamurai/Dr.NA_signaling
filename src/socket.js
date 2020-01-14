ee9ba2b0f36dfc5069f511dde7b75985cec914ed
Aoi Satonaka <fireball0821@icloud.com>
2020-01-15 00:57:44 +0900

    [fix] typo config propaty

--- src/socket.js
+++ src/socket.js
@@ -43,5 +43,5 @@ wsServer.on('connection', function connection(sock, req) {
     });
 });
 
-httpsServer.listen(config.get('port'));
-srv.log('Server Start on address - ' + ip.address() + ' - port - ' + config.get('port') + ' - ');
+httpsServer.listen(config.get('Port'));
+srv.log('Server Start on address - ' + ip.address() + ' - port - ' + config.get('Port') + ' - ');
